c98168be429df304fc48822ee1bb286ea9af9d54
Legend <legend.begins@yahoo.com>
2020-05-25 16:42:29 -0500

    Converted to Iterative Parser
    Removed arcane magic and unneeded complexity

--- bbcode.py
+++ bbcode.py
@@ -5,10 +5,6 @@ stopList = ['code']
 tagList = ['b','i', 'quote']
 #Regular tags with opening and closing versions
 
-RESERVED_TAG = 'bbcode'
-#Feel free to change if you want it
-
-
 def onMisalignedTags():
 	print('Some tags appear to be misaligned')
 
@@ -20,7 +16,6 @@ def processTag(tag, data = None):
 
 
 tagList.extend(stopList)
-tagList.append(RESERVED_TAG)
 
 tagRegex = '(?<=\[)(' + '|'.join(tagList) + ')(\\s*=.*?)?(?=\])'
 endTagRegex = '(?<=\[/)(' + '|'.join(tagList) + ')(?=\])'
@@ -46,74 +41,72 @@ def findClosingNoParse(tag, message):
 	if not endResult:
 		#If the noparse tag isn't closed
 		onMisalignedTags()
-		return 0, message
+		return len(message), message
 	else:
 		return endResult.start() - 2, message[:endResult.start() - 2]
 		#Return the content just before the closing tag starts (accounting for '[/')
 
+#TO DO: Ignore case (could just set tag variable to lower?)
+#TO DO: Escape evil chars
+#TO DO: Add tags that don't include closing tags (e.g. [*])
+#TO DO: @mentions
+#One option for guessing what the user meant: Add appropriate tag and check if it fixes the error. If not, delete tag and check. If not, change tag into appropriate tag and check.
+
 def parseBBCode(message):
 	contentEnd = 0
-	#This value only changes if we find a deeper tag set. It is returned 
-	tag = ''
-	endTag = ''
+	#This value changes as we scan through the tag set
 	rebuiltString = ''
+	tagStack = []
 	while True:
 		#Loop until we have traversed every tag in this level
 		result = re.search(tagRegex, message[contentEnd:])
 		#We measure from contentEnd because we need to know where to search from when two tags are embedded on the same level
 		endResult = re.search(endTagRegex, message[contentEnd:])
 		#We grab both the next start and end tags and see which comes first
-		if result:
-			hasInnerTags = False
-			if not endResult or endResult.start() > result.start():
-				#If we find another opening tag before a closing one, we need to go a step deeper
-				hasInnerTags = True
-				tag, tagData = getTagAndOptionalData(result)
-				#If there is no = in the tag, tagData will be null. If there is but it's unnecessary (e.g. [b=Random]), it will be ignored.
-				rebuiltString += message[contentEnd: contentEnd + result.start() - 1] + processTag(tag, tagData)
-				#Add everything up to and including the tag to the rebuilt string. We have to remember that results is always going to be offset by contentEnd
-				contentEnd += result.end() + 1
-				if tag not in stopList:
-					endIndex, embeddedContent = parseBBCode(message[contentEnd:])
-					contentEnd += endIndex
-					rebuiltString += embeddedContent
-				else:
-					#If we encounter a noparse tag
-					endIndex, embeddedContent = findClosingNoParse(tag, message[contentEnd:])
-					contentEnd += endIndex
-					rebuiltString += embeddedContent
-					#We have to add the index of the result as well
-				endResult = re.search(endTagRegex, message[contentEnd:])
-				#Search for ending tag after our enclosed data ends. Update endResult with the new tag after embedded content
-
-			if endResult:
-				if hasInnerTags:
-					rebuiltString += message[contentEnd:contentEnd + endResult.start() - 2]
-					#Embed everything up to our closing tag in the resulting content
-					contentEnd += endResult.end() + 1
-					#We have to add 1 to account for close bracket and shift our index forward
-					endTag = endResult.groups()[0]
-					#We only care about updating the end tag if we need to check that it matches for inner tags.
-					rebuiltString += processCloseTag(endTag, None)
-				else:
-					#If another opening tag exists later but 
-					return contentEnd, rebuiltString
-				
-			if endTag != tag:
-				#If tag is an empty string, endTag is necessarily also an empty string because result is necessarily null
-				#Otherwise, it means there's a deeper set of tags and the end tag doesn't match the start tag
+		if result and (not endResult or endResult.start() > result.start()):
+			#If our next tag is an open tag
+			tag, tagData = getTagAndOptionalData(result)
+			tagStack.append(tag)
+			#If there is no = in the tag, tagData will be null. If there is but it's unnecessary (e.g. [b=Random]), it will be ignored.
+			rebuiltString += message[contentEnd: contentEnd + result.start() - 1]
+			rebuiltString += processTag(tag, tagData)
+			#Add everything up to and including the tag to the rebuilt string. We have to remember that results is always going to be offset by contentEnd
+			contentEnd += result.end() + 1
+			if tag in stopList:
+				#If we encounter a noparse tag
+				endIndex, embeddedContent = findClosingNoParse(tag, message[contentEnd:])
+				contentEnd += endIndex
+				rebuiltString += embeddedContent
+				#We have to add the index of the result as well
+		elif endResult:
+			#If the next tag is a closing one
+			rebuiltString += message[contentEnd:contentEnd + endResult.start() - 2]
+			endTag = endResult.groups()[0]
+			parserEnd = endResult.end() + 1
+			if not tagStack:
+				#If this is an unpaired closing tag, treat it as text and keep going
+				rebuiltString += message[contentEnd:parserEnd]
+				contentEnd += parserEnd
+				continue
+			elif endTag != tagStack[-1]:
+				#If our tags don't match
 				onMisalignedTags()
-				return contentEnd, rebuiltString
+				endTag = tagStack[-1]
+			rebuiltString += processCloseTag(endTag, None)
+			contentEnd += parserEnd
+			tagStack.pop()
 		else:
-			#If we didn't find any more opening tags, we've reached the end of the tags on this level (and by definition, lower levels)
-			return contentEnd, rebuiltString
-			#Return the index of where this tag's scope ends
+			#If we're out of tags
+			if tagStack:
+				#If we don't have enough closing tags
+				onMisalignedTags()
+			rebuiltString += message[contentEnd:]
+			break
+	return rebuiltString
 
-def evaluateBBCode(codeString):
-	return parseBBCode('[' + RESERVED_TAG + ']' + codeString + '[/' + RESERVED_TAG + ']')[1]
 
 	
-toEvaluate = '[i][i][quote=@LegendBegins]Hello![/quote][/i][b]Hello![/b][/i][b]Hello![/b] sdfs'
-print(evaluateBBCode(toEvaluate))
+toEvaluate = '[i][b][quote=@LegendBegins]Hello![/quote][/b][b]Hello![/b][/i][b]Hello![/b] sdfs'
+print(parseBBCode(toEvaluate))
 toEvaluate = 'first[b]second[/b]third[i]fourth[/i]fifth'	
-print(evaluateBBCode(toEvaluate))
+print(parseBBCode(toEvaluate))
